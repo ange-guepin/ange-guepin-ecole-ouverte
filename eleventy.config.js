351f0c8c5bb93551e1d4192fc0dc8c5fe4a9673f
ArnaudBan <arnaud@banvillet.fr>
2026-01-30 09:15:57 +0100

    fix: update eleventy and sass

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,22 +1,21 @@
-module.exports = function (eleventyConfig) {
+import { DateTime } from "luxon";
+
+export default function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/js");
   eleventyConfig.addPassthroughCopy("./src/admin");
   eleventyConfig.addPassthroughCopy("./src/medias");
 
-  const { DateTime } = require("luxon");
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).setLocale('fr').toLocaleString(DateTime.DATE_FULL);
   });
 
-  // eleventyConfig.addPassthroughCopy("./src/img");
   return {
     dir: {
       input: "src",
       output: "public",
+      includes: "_includes",
     },
   };
-
-};
-
+}
